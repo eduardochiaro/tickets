01f0bed286de69d798947fc1aa08bd3b111bffcd
Eduardo Chiaro <e.chiaro@gmail.com>
2023-05-22 11:32:45 -0700

    icons

--- src/components/ShowProviders.tsx
+++ src/components/ShowProviders.tsx
@@ -1,6 +1,20 @@
 'use client';
 
+import GitHub from '@/icons/Github';
+import Google from '@/icons/Google';
 import { getProviders, signIn } from 'next-auth/react';
+
+
+const LogoProvider = ({ id }: { id: string }) => {
+  switch (id) {
+    case 'github':
+      return <GitHub className="w-5" />;
+    case 'google':
+      return <Google className="w-5" />;
+    default:
+      return <></>;
+  }
+};
 
 const ShowProviders = async function ShowProviders() {
   const providers = await getProviders();
@@ -9,7 +23,8 @@ const ShowProviders = async function ShowProviders() {
       {providers &&
         Object.values(providers).map((provider) => (
           <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)} className="btn bg-gray-900 text-gray-50 ring-gray-700 !text-xl !p-3">
+            <button onClick={() => signIn(provider.id)} className="flex item-center gap-4 font-mono btn bg-gray-900 text-gray-50 !border-gray-600 !text-xl">
+							<LogoProvider id={provider.id}/>
               Sign in with {provider.name}
             </button>
           </div>
