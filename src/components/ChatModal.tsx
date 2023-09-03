d3e680ce26d31808d842de985fc2842e351fbab8
Eduardo Chiaro <e.chiaro@gmail.com>
2023-09-02 21:57:43 -0700

    chat style

--- src/components/ChatModal.tsx
+++ src/components/ChatModal.tsx
@@ -11,6 +11,7 @@ import ExtendedUser from '@/models/ExtendedUser';
 import moment from "moment";
 import { convertToHTML } from '@/utils/convertToHTML';
 import axios from "axios";
+import classNames from '../utils/classNames';
 
 const AlwaysScrollToBottom = () => {
   const elementRef = useRef<null | HTMLDivElement>(null);
@@ -68,17 +69,46 @@ export default function ChatModal({ showChatModal, onClose }: { showChatModal: a
   };
 
   const Message = ({ message }: { message: any }) => {
-    if (parseInt(user?.id) === message.userId) {
-      return (
-        <div className="flex justify-end mb-4">
-        <div className="flex flex-col">
-          <div className="flex justify-between gap-1">
-            <div className="text-xs pl-3">{moment(message.createdAt).fromNow()}</div>
-            <div className="text-xs pr-5">{message.user.name}</div>
+    // if date is different from previous message, show date
+
+    const isDifferentDate = (message: any) => {
+      const index = messages.indexOf(message) > 0 ? messages.indexOf(message) - 1 : 0;
+      if (index == 0) {
+        return true;
+      }
+      const previousMessage = messages[index];
+      if (previousMessage) {
+        const previousMessageDate = moment(previousMessage.createdAt).format('DD/MM/YYYY');
+        const currentMessageDate = moment(message.createdAt).format('DD/MM/YYYY');
+        return previousMessageDate !== currentMessageDate;
+      }
+      return true;
+    };
+
+    return (
+      <>
+      {isDifferentDate(message) && (
+        <div className="flex items-center justify-center mb-4">
+          <hr className="border-dotted border-gray-500 grow" />
+          <div className="text-xs px-2 py-1 rounded-full">
+            {moment(message.createdAt).format('MMMM Do, YYYY')}
           </div>
-          <div className="py-3 px-4 bg-green-600 rounded-bl-xl rounded-tl-xl rounded-br-xl text-white" dangerouslySetInnerHTML={{
-            __html: convertToHTML(message.message)
-          }}></div>
+          <hr className="border-dotted border-gray-500 grow" />
+        </div>
+      )}
+      {(parseInt(user?.id) === message.userId) ? (
+        <div className="flex justify-end mb-4 group">
+          <div className="flex flex-col">
+            <div className="flex justify-between gap-4">
+              <div className="text-xs pl-3">
+                <div className="group-hover:hidden">{moment(message.createdAt).fromNow()}</div>
+                <div className="hidden group-hover:block">{moment(message.createdAt).format('MM/DD/YYYY h:mm:ss a')}</div>
+              </div>
+              <div className="text-xs pr-5">{message.user.name}</div>
+            </div>
+            <div className="py-3 px-4 bg-green-600 rounded-bl-xl rounded-tl-xl rounded-br-xl text-white" dangerouslySetInnerHTML={{
+              __html: convertToHTML(message.message)
+            }}></div>
           </div>
           <Image
             className="h-8 w-8 rounded-full ring-2 ring-gray-200 hover:ring-sky-400 -ml-3 z-10"
@@ -89,28 +119,31 @@ export default function ChatModal({ showChatModal, onClose }: { showChatModal: a
             height={100}
           />
         </div>
-      );
-    }
-    return (
-      <div className="flex justify-start mb-4">
-        <Image
-          className="h-8 w-8 rounded-full ring-2 ring-gray-200 hover:ring-sky-400 -mr-3 z-10"
-          src={message.user.image}
-          alt={message.user.name}
-          title={message.user.name}
-          width={100}
-          height={100}
-        />
-        <div className="flex flex-col">
-          <div className="flex justify-between gap-1">
-            <div className="text-xs pl-5">{message.user.name}</div>
-            <div className="text-xs pr-3">{moment(message.createdAt).fromNow()}</div>
+      ) : (
+        <div className="flex justify-start mb-4 group">
+          <Image
+            className="h-8 w-8 rounded-full ring-2 ring-gray-200 hover:ring-sky-400 -mr-3 z-10"
+            src={message.user.image}
+            alt={message.user.name}
+            title={message.user.name}
+            width={100}
+            height={100}
+          />
+          <div className="flex flex-col">
+            <div className="flex justify-between gap-4">
+              <div className="text-xs pl-5">{message.user.name}</div>
+              <div className="text-xs pr-3">
+                <div className="group-hover:hidden">{moment(message.createdAt).fromNow()}</div>
+                <div className="hidden group-hover:block">{moment(message.createdAt).format('MM/DD/YYYY h:mm:ss a')}</div>
+              </div>
+            </div>
+            <div className="py-3 px-4 bg-gray-500 rounded-br-xl rounded-tr-xl rounded-bl-xl text-white" dangerouslySetInnerHTML={{
+              __html: convertToHTML(message.message)
+            }}></div>
           </div>
-          <div className="py-3 px-4 bg-gray-500 rounded-br-xl rounded-tr-xl rounded-bl-xl text-white" dangerouslySetInnerHTML={{
-            __html: convertToHTML(message.message)
-          }}></div>
         </div>
-      </div>
+      )}
+    </>
     );
   };
 
@@ -139,13 +172,13 @@ export default function ChatModal({ showChatModal, onClose }: { showChatModal: a
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <Dialog.Panel className="w-full max-w-3xl rounded-lg bg-white dark:bg-gray-800 p-8 my-20 h-3/4 flex flex-col">
-              <h3 className="group text-xl flex items-center gap-1">
+            <Dialog.Panel className="w-full max-w-3xl rounded-lg bg-white dark:bg-gray-800 my-20 h-3/4 flex flex-col">
+              <h3 className="px-8 pt-8 group text-xl flex items-center gap-1">
                 <DocumentIcon className="h-4" />
                 Chat
               </h3>
-              <Dialog.Title className="text-3xl border-b border-gray-500 pb-4 group">{issueData?.title}</Dialog.Title>
-              <Dialog.Description className="pt-5 grow flex flex-col gap-4 px-2 overflow-y-auto" as="div">
+              <Dialog.Title className="px-8 text-3xl border-b border-gray-500 pb-4 group">{issueData?.title}</Dialog.Title>
+              <Dialog.Description className="p-8 grow flex flex-col gap-4 overflow-y-auto" as="div">
                 {isLoadingMessages && (
                   <div className="flex items-center justify-center gap-2">
                     <SpinnerIcon className="animate-spin h-5 w-5" />
@@ -160,10 +193,10 @@ export default function ChatModal({ showChatModal, onClose }: { showChatModal: a
                 {messages && messages.map((message: any) => <Message key={message.id} message={message} />)}
                 <AlwaysScrollToBottom />
               </Dialog.Description>
-              <div className="border-t border-gray-500 flex items-center gap-2">
-                <input type="text" className="grow p-2 bg-transparent" placeholder="Type a message..." value={message} onChange={(e) => handeChanges(e) } />
-                <button onClick={() => SendMessageAction() }>
-                  <PaperAirplaneIcon className="h-5 w-5 text-green-600 hover:text-emerald-500" />
+              <div className="px-8 py-2 border-t border-gray-500 flex items-center gap-2 bg-gray-100 dark:bg-gray-700 rounded-b-lg">
+                <input type="text" className="grow p-2 focus:outline-none bg-transparent" placeholder="Type a message..." value={message} onChange={(e) => handeChanges(e) } />
+                <button disabled={message.length <= 3} className={classNames(message.length <= 3 ? `text-gray-400` : `group text-green-600 hover:text-emerald-500`)} onClick={() => SendMessageAction() }>
+                  <PaperAirplaneIcon className="h-7 w-7 transition-all group-hover:-rotate-[25deg]" />
                 </button>
               </div>
             </Dialog.Panel>
